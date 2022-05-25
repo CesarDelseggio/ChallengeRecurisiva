f14867ced026b3674a19b0cfd8fc2b66b84e491b
César <cesar_delse@hotmail.com>
2022-05-25 13:48:49 -0300

    Move Specification and EntityBase to common project Change LogServices to use Specification. Change references to common project

---
+++ ChallengeRecursiva.Business/Specifications/Logs/LogFilterByTitleSpec.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeRecursiva.Business.Specifications.Logs
+{
+    internal class LogFilterByTitleSpec : Specification<Log>
+    {
+    }
+}
