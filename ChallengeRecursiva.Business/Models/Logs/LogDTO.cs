c9437e4d86ad7be27ef14638e5e72cafe3cee04d
César <cesar_delse@hotmail.com>
2022-05-25 11:56:41 -0300

    Add DTO for log entity. Add an configura AutoMapper Add Log Page empty

---
+++ ChallengeRecursiva.Business/Models/Logs/LogDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeRecursiva.Business.Models.Logs
+{
+    public class LogDTO : BaseDTO
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+    }
+}
