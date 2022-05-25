2f0a6f7eeeeca75c8cd5fd3f2d74a047075b097f
César <cesar_delse@hotmail.com>
2022-05-25 10:57:34 -0300

    Add BusinessConfig and DataAccessConfig to configura DependencyInyection. Add defaul connection string in appsettings

---
+++ ChallengeRecursiva.Business/BusinessConfig.cs
@@ -0,0 +1,21 @@
+﻿using ChallengeRecursiva.Business.Interfaces;
+using ChallengeRecursiva.Business.Services;
+using ChallengeRecursiva.DataAccess.Data.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeRecursiva.Business
+{
+    public static class BusinessConfig
+    {
+        public static void AddBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDataAccessServices(configuration);
+
+            services.AddScoped<IService<Log>, LogService>();
+        }
+    }
+}
---
+++ ChallengeRecursiva.DataAccess/DataAccessConfig.cs
@@ -0,0 +1,24 @@
+﻿using ChallengeRecursiva.DataAccess;
+using ChallengeRecursiva.DataAccess.Data.Models;
+using ChallengeRecursiva.DataAccess.Interfaces;
+using ChallengeRecursiva.DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeRecursiva.Business
+{
+    public static class DataAccessConfig
+    {
+        public static void AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionStrings")));
+
+            services.AddScoped<IRepository<Log>, Repository<Log>>();
+        }
+    }
+}
